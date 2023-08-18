fd0c7a7a2a7be852dd225ff6a6468a65628c7ffd
Michael Citi <michael-29390034+michael-citi@users.noreply.github.com>
2023-08-18 16:30:23 -0700

    stuff

---
+++ Components/ApiTestGrid.razor.cs
@@ -0,0 +1,82 @@
+﻿using System.Net.Mime;
+using System.Text;
+using System.Text.Json;
+using BlazorBootstrap;
+using ComponentTest.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace ComponentTest.Components;
+
+public partial class ApiTestGrid : ComponentBase
+{
+    private ICollection<Post>? Posts { get; set; }
+    private Post? SelectedPost { get; set; }
+    private string? NewPostContent { get; set; }
+    
+    private Grid<Post> ApiGrid { get; set; }
+    
+    [Parameter] public string? ApiGet { get; set; }
+    
+    [Inject] public IHttpClientFactory ClientFactory { get; set; }
+    
+    private async Task<GridDataProviderResult<Post>> PostGridDataProvider(GridDataProviderRequest<Post> request)
+    {
+        var httpRequest = new HttpRequestMessage(HttpMethod.Get, ApiGet);
+
+        var client = ClientFactory.CreateClient();
+
+        var response = client.Send(httpRequest);
+
+        if (response.IsSuccessStatusCode)
+            await GetPosts(response);
+
+        return new GridDataProviderResult<Post> { Data = Posts ?? new List<Post>() };
+    }
+
+    private Task OnSelectedItemsChanged(HashSet<Post> posts)
+    {
+        
+        return Task.CompletedTask;
+    }
+
+    protected async Task OnClick_AddPost()
+    {
+        if (!string.IsNullOrEmpty(NewPostContent))
+        {
+            var post = new Post { Content = NewPostContent };
+            
+            var content = new StringContent(
+                JsonSerializer.Serialize(post),
+                Encoding.UTF8,
+                MediaTypeNames.Application.Json);
+
+            var client = ClientFactory.CreateClient();
+
+            var response = await client.PostAsync(ApiGet, content);
+
+            if (response.IsSuccessStatusCode)
+                await GetPosts(response);
+        }
+    }
+
+    protected async Task OnClick_DeletePosts()
+    {
+        if (SelectedPost != null)
+        {
+            var client = ClientFactory.CreateClient();
+            
+            var httpRequest = new HttpRequestMessage(HttpMethod.Delete, $"{ApiGet}/{SelectedPost.Id}");
+
+            var response = client.Send(httpRequest);
+
+            await GetPosts(response);
+        }
+    }
+
+    private async Task GetPosts(HttpResponseMessage response)
+    {
+        using var readStream = response.Content.ReadAsStreamAsync();
+        Posts = await JsonSerializer.DeserializeAsync<ICollection<Post>>(readStream.Result);
+        await ApiGrid.RefreshDataAsync();
+    }
+}
---
+++ Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ComponentTest.Models;
+
+public class Post
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+    
+    [JsonPropertyName("content")]
+    public string? Content { get; set; }
+    
+    [JsonPropertyName("dateCreated")]
+    public DateTime DateCreated { get; set; }
+    
+    [JsonPropertyName("lastModified")]
+    public DateTime LastModified { get; set; }
+}
